02b6f02c5bedd28093a49d7f346a3177a18a2316
冯 <2789083329@qq.com>
2024-12-13 16:21:36 +0800

    支持带参构造器

--- ConstructorRoslynGenerator.cs
+++ ConstructorRoslynGenerator.cs
@@ -17,16 +17,21 @@ namespace MinimalisticWPF.Generator
 
         private void GenerateConstructor(INamedTypeSymbol namedTypeSymbol, bool isAop, bool isDynTheme)
         {
-            var methods = namedTypeSymbol.GetMembers().OfType<IMethodSymbol>().Where(m => m.GetAttributes().Any(att => att.AttributeClass?.Name == "VMInitializationAttribute"));
+            var methods = namedTypeSymbol.GetMembers()
+                .OfType<IMethodSymbol>()
+                .Where(m => m.GetAttributes().Any(att => att.AttributeClass?.Name == "VMInitializationAttribute"))
+                .ToList();
+
             StringBuilder builder = new StringBuilder();
             var strAop = $"IAop{namedTypeSymbol.Name}In{namedTypeSymbol.GetNameSpaceWithOutDot()}";
             if (isAop)
             {
-                builder.AppendLine($$"""      
+                builder.AppendLine($$"""
                                            public {{strAop}} Proxy { get; private set; }
                                      """);
                 builder.AppendLine();
             }
+
             builder.AppendLine($"      public {namedTypeSymbol.Name} ()");
             builder.AppendLine("      {");
             if (isAop)
@@ -37,11 +42,39 @@ namespace MinimalisticWPF.Generator
             {
                 builder.AppendLine($"         this.ApplyGlobalTheme();");
             }
-            foreach (var method in methods)
+            foreach (var method in methods.Where(m => !m.Parameters.Any()))
             {
                 builder.AppendLine($"         {method.Name}();");
             }
             builder.AppendLine("      }");
+
+            var groupedMethods = methods.Where(m => m.Parameters.Any()).GroupBy(m =>
+                string.Join(",", m.Parameters.Select(p => p.Type.ToDisplayString())));
+
+            foreach (var group in groupedMethods)
+            {
+                var parameters = group.Key.Split(',');
+                var parameterList = string.Join(", ", group.First().Parameters.Select(p => $"{p.Type.ToDisplayString()} {p.Name}"));
+                var callParameters = string.Join(", ", group.First().Parameters.Select(p => p.Name));
+
+                builder.AppendLine();
+                builder.AppendLine($"      public {namedTypeSymbol.Name} ({parameterList})");
+                builder.AppendLine("      {");
+                if (isAop)
+                {
+                    builder.AppendLine($"         Proxy = this.CreateProxy<{strAop}>();");
+                }
+                if (isDynTheme)
+                {
+                    builder.AppendLine($"         this.ApplyGlobalTheme();");
+                }
+                foreach (var method in group)
+                {
+                    builder.AppendLine($"         {method.Name}({callParameters});");
+                }
+                builder.AppendLine("      }");
+            }
+
             Constructor = builder.ToString();
         }
     }
